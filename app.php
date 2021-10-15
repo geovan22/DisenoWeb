42974a0be7a2596d9da0d537d11ce46692a2c0a1
geovan22 <geovan221315@hotmail.com>
2021-10-14 20:37:16 -0600

    array
    cambios

---
+++ app.php
@@ -0,0 +1,113 @@
+<?php
+    //var_dump($_POST);
+    
+    $n1=$_POST['numero1'];
+    $n2=$_POST['numero2'];
+    $op=$_POST['operacion'];
+    $resultado=0;
+    
+    /*
+    
+    if($op=='s')
+    {
+        $resultado=$n1+$n2;
+    }//end if
+    
+    else if($op=='r')
+    {
+        $resultado=$n1-$n2;
+    }
+    
+    else if($op=='m')
+    {
+        $resultado=$n1*$n2;
+    }
+    
+    else if($op=='d')
+    {
+        $resultado=$n1/$n2;
+    }
+    
+    echo "<h3> el resultado es: ".$resultado."</h3>";
+    
+    
+    for($x=$n1;$x<=$n2;$x=$x+2)//$x=$x+1
+    {
+        echo $x."<br>";
+    }
+    
+    echo "<br>"."<br>"."<br>";
+    
+    $y=15;
+    while($y<15)
+    {
+        echo $y."<br>";
+        $y++;
+    }
+    echo "<br>"."<br>"."<br>";
+   
+    do
+    {
+        echo $y."<br>";
+        $y++;
+    }while($y<15);
+    
+    
+    */
+    
+    
+    echo "<br>"."<br>"."<br>";
+    $arr=array(0,1,2,"hola",0.55,'x',"adios");
+    
+    var_dump($arr);
+    
+    for($x=0;$x<count($arr);$x++)
+    {
+        echo $arr[$x]."<br>";
+    }
+    
+    echo "<br>"."<br>"."<br>";
+    
+    $frutas=array(
+        "rojas"=>"manzana",
+        "amarillas"=>"banano",
+        "estado"=>true,
+        array(
+                0,1,2,3
+        )
+    );
+    
+    var_dump($frutas);
+    
+    //echo $frutas['amarillas'];
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+?>
